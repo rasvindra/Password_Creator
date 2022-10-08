4f7dd5b3a9f57135753beb1fdb765243f3eeba30
Ravi Nagi <rasvindra@hotmail.com>
2022-10-08 13:18:52 -0700

    polished eventlistner code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 
-const createPassword = document.querySelector("#createpassword")
 
-const lwrLetterChar =["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-const upprLetterChar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const numChar =["0","1","2","3","4","5","6","7","8","9"];
-const specialLetterChar =["~","`","!","@","#","$","%","^","&","*","(",")","_","+","-","=","{","}","[","]",":",";","'","<",">","?",".","/","|",'"',","];
+var lwrLetterChar =["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upprLetterChar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numChar =["0","1","2","3","4","5","6","7","8","9"];
+var specialLetterChar =["~","`","!","@","#","$","%","^","&","*","(",")","_","+","-","=","{","}","[","]",":",";","'","<",">","?",".","/","|",'"',","];
+
 
 function UserInput() {
 var upper = prompt("Would you like to include Upper Case Letters In your password? Please enter Y for yes or N or no.")
@@ -24,7 +24,7 @@ if (number !== "Y" && number !== "N")
 alert("Sorry not a Valid option. Please enter Y or N for your choice")
 
 var passwordLength = parseInt( prompt(
-    "How many charaters would you like your password to be? Minimum length is 8 characters and no maximum length can be 128 characters"
+    "How many charaters would you like your password to be? Minimum length is 8 characters and maximum length can be 128 characters"
   ));
 if (passwordLength < 8 || passwordlength > 128)
   alert("Sorry Not a valid choice! Please select a number between 8 and 128");
@@ -48,7 +48,8 @@ else {
             result = result.concat(numChar)
         }
 
-        function nameoffunctionIwillusetocreatethepassword (passwordLength, Result) 
+        // function nameoffunctionIwillusetocreatethepassword (passwordLength, Result) 
+    }
 }
 
-}
+document.querySelector("#createpassword").addEventListener("click",UserInput)
