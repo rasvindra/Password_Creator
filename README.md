62893f7cd45b1fdad315276be1039ec1d4fe829a
Ravi Nagi <rasvindra@hotmail.com>
2022-10-11 13:42:23 -0700

    created readme. finalized JS and CSS file

---
+++ README.md
@@ -0,0 +1,41 @@
+
+
+# Password_Creator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+* [Description](#description)
+* [Install](#install)
+* [Contribution](#contribution)
+* [Usage](#usage)
+* [Testing](#testing)
+* [Questions](#questions)
+* [License](#license)
+
+
+## Description
+A simple app that displays knowledge of iteration through arrays to create an unsecure password based on user inputs
+
+## Install
+Nothing needed to install. Just need a webrowser and internet connection
+
+## Contribution
+NaN
+
+## Usage
+Public
+
+## Testing
+NaN
+
+## Questions
+Please reach out to me though my GitHub or via E-mail with any questions or concerns you may have <br/>
+rasvindra@hotmail.com <br/>
+[github.com/rasvindra](https://github.com/rasvindra)
+
+## License
+MIT
+
+https://github.com/rasvindra/Password_Creator
+
+![](assets/images/Password_screenshot.PNG.PNG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 
+// I created global arrays before starter code was available
+const lwrLetterChar =["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const upprLetterChar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const numChar =["0","1","2","3","4","5","6","7","8","9"];
+const specialLetterChar =["~","`","!","@","#","$","%","^","&","*","(",")","_","+","-","=","{","}","[","]",":",";","'","<",">","?",".","/","|",'"',","];
 
-var lwrLetterChar =["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upprLetterChar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numChar =["0","1","2","3","4","5","6","7","8","9"];
-var specialLetterChar =["~","`","!","@","#","$","%","^","&","*","(",")","_","+","-","=","{","}","[","]",":",";","'","<",">","?",".","/","|",'"',","];
-
-
+// function that starts prompts for parameters once button is pushed
 function UserInput() {
     while(upper !== "Y" && upper !== "N") {
 var upper = prompt("Would you like to include Upper Case Letters In your password? Please enter Y for yes or N or no.")
@@ -33,11 +33,13 @@ var passwordLength = parseInt( prompt(
   ));
 if (passwordLength < 8 || passwordLength > 128) {
   alert("Sorry Not a valid choice! Please select a number between 8 and 128");
-  return
+}
+// Commented out this validation check after adding while loops
 // if (upper !=="Y" && lower !=="Y" && special !=="Y" && number !=="Y") {
 // alert("You decided to have no values for your password. Please enter at least one value to create your password")
-// }
-}
+//}
+
+// calls the function that creates a new array named result with parameters user selected
 else {
     var result = []
         if (upper==="Y"){
@@ -54,25 +56,25 @@ else {
         }
 
         begintheProcess(passwordLength, result) 
-        console.log(result, "test 111111111111111111111")
-        console.log(numChar,"test 33333333333333333333")
-        console.log(specialLetterChar, "test 444444444444444444")
+        // console.log(result, "test 111111111111111111111")
+        // console.log(numChar,"test 33333333333333333333")
+        // console.log(specialLetterChar, "test 444444444444444444")
     }
 }
 
-
+// The function that iterates through array to create a Unsecure Password
  function begintheProcess(passwordLength, result){
     var newPassword = ""
     for(var i=0; i < passwordLength; i++){
 
-
         newPassword=newPassword+result[Math.floor(Math.random()* result.length)]
         
-        console.log(newPassword) //test 222222
-        console.log(result, 'test555555555555555555')
+        // console.log(newPassword, "test 22222222222")
+        // console.log(result, "test555555555555555555")
     }
     var displayPassword = document.querySelector("#password")
     displayPassword.value = newPassword
  }
-
+ 
+// Event listner that responds to button click and calls userinput function
 document.querySelector("#generate").addEventListener("click",UserInput)
